b6e415fb6afb9021fa32797551cf75c9f858a9ce
Ycow <skypnal12@gmail.com>
2025-08-04 11:44:50 +0900

    refactor: 재사용 Input 컴포넌트 생성

---
+++ src/advanced/components/ui/Input.tsx
@@ -0,0 +1,44 @@
+import {
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
+  memo,
+  PropsWithChildren,
+} from "react";
+
+// 상태공유 필요 시 Root에 context 추가해서 확장 가능
+
+const InputRoot = memo(({ children }: PropsWithChildren) => {
+  return <div>{children}</div>;
+});
+
+interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
+  children: React.ReactNode;
+}
+
+const Label = memo(({ children, className, ...props }: LabelProps) => {
+  return (
+    <label
+      className={`block text-sm font-medium text-gray-700 mb-1 ${className}`}
+      {...props}
+    >
+      {children}
+    </label>
+  );
+});
+
+interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+const Field = ({ className, ...props }: FieldProps) => {
+  return (
+    <input
+      className={`w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 px-3 py-2 border ${className}`}
+      {...props}
+    />
+  );
+};
+
+// 컴파운드 컴포넌트 조립
+export const Input = Object.assign(InputRoot, {
+  Label,
+  Field,
+});
