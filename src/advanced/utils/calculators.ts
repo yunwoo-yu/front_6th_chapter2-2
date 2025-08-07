3e7a6cf666d59310d606e995e1b838143ee8b2ac
Ycow <skypnal12@gmail.com>
2025-08-07 16:16:11 +0900

    fix: 역할 주석 변경

--- src/advanced/utils/calculators.ts
+++ src/advanced/utils/calculators.ts
@@ -1,7 +1,7 @@
 // 곱셈 계산
 export const multiply = (a: number, b: number): number => a * b;
 
-// 할인 요금 계산
+// 할인율 계산
 export const calculatePercentageChange = (
   originalAmount: number,
   discountedAmount: number
